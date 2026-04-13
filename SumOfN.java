a7d7c6838903ab9f3d951a299c85b2a11c0572c9
ManishGihub <102681545+ManishGihub@users.noreply.github.com>
2026-04-13 22:12:50 +0530

    Update SumOfN.java

--- SumOfN.java
+++ SumOfN.java
@@ -10,8 +10,14 @@ public class SumOfN {
         }
         sum += i;
         Son(i+1,n,sum);
+        System.out.println(i); //Optional to check the recursion
     }
     public static void main(String[] args) {
         Son(1, 5, 0);
     }
 }
+// 15
+// 4
+// 3
+// 2
+// 1
