00956d388d922d14830e8486d2eaac8b1808c118
ManishGihub <102681545+ManishGihub@users.noreply.github.com>
2026-04-14 22:19:54 +0530

    Done

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class Factorial {
+    public static int Factorialn(int n){
+        if(n == 1 || n == 0){
+            return 1;
+        }
+
+        int fact_nm1 = Factorialn(n-1);
+        int fact_n = n * fact_nm1;
+
+        return fact_n;
+    }
+    public static void main(String[] args) {
+        System.out.println(Factorialn(5));
+    }
+}
+// 120
