22c5850f924fc5110696ce74659cb39960e27a8e
ManishGihub <102681545+ManishGihub@users.noreply.github.com>
2026-04-16 22:48:54 +0530

    Done

---
+++ XPowNStackHeightlogn.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class XPowNStackHeightlogn {
+    public static int xpn(int x,int n){
+        if(n == 0){
+            return 1;
+        }
+        if(x == 0){
+            return 0;
+        }
+        if(n%2 == 0){
+            return xpn(x, n/2) * xpn(x, n/2);
+        }else{
+            return xpn(x, n/2) * xpn(x, n/2) * x;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(xpn(2, 5));
+    }
+}
+// 32
